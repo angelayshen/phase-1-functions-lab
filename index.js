12a1206b43f4bb65af2d642f4617e24d3305dd37
Angela Shen <angelayshen@gmail.com>
2023-05-01 15:41:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    } else if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    } else {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    }
+  }
